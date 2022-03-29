a55026e2cc8c49b45e27ba8aaccd09f349aca46a
juanasalamanco <juanasalamanco@gmail.com>
2022-03-29 08:58:23 -0300

    Ejercicio5 finish

---
+++ Assets/Ejercicio5.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    public int num1 = 0;
+    public int num2 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int resultado = (num1 / num2);
+        if (num2 == 0)
+        {
+            Debug.Log("No se puede dividir por 0");
+        }
+        else
+        {
+            Debug.Log(+num1+ " / " +num2+ " = " + resultado);
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
