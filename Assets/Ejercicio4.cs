ee64db363ef30f7b3b36e9131dc2a37f8ef08d48
juanasalamanco <juanasalamanco@gmail.com>
2022-03-29 08:49:50 -0300

    Ejercicio4 finish

---
+++ Assets/Ejercicio4.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio4 : MonoBehaviour
+{
+    public int num1 = 0;
+    public int num2 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int suma = (num1 + num2);
+        Debug.Log("La suma entre " +num1+ " y " +num2+ " da como resultado " +suma+ ".");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
