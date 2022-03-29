5cc9bb4f789f1a44d2bdbd936371247288c5f8d7
juanasalamanco <juanasalamanco@gmail.com>
2022-03-29 08:30:44 -0300

    Ejercicio2 finish

---
+++ Assets/Ejercicio2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+
+        Debug.Log (num1 + num2);
+        Debug.Log(num1 * num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
