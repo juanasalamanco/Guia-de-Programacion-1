1ffb9aaf4981cb8ed05a66022056fd4914f34f8f
juanasalamanco <juanasalamanco@gmail.com>
2022-03-29 08:40:34 -0300

    Ejercicio3 finish

---
+++ Assets/Ejercicio3.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio3 : MonoBehaviour
+{
+    public string txt = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola " +txt+ ".");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
