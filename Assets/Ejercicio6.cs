f08a4bf180c9771dc79bc6a1971ed918cf359fdc
juanasalamanco <juanasalamanco@gmail.com>
2022-03-30 15:23:58 -0300

    Ejercicio6 finish

---
+++ Assets/Ejercicio6.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6 : MonoBehaviour
+{
+    public int num1 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == 0)
+        {
+            Debug.Log("El valor tiene que ser mayor a 0");
+        }
+        else if ((num1 % 2) == 0)
+        {
+            Debug.Log("El numero es par");
+        }
+        else
+        {
+            Debug.Log("El numero es impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
